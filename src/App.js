b2e56944a8491f95a9ab9a44e5db79322dd52fa2
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2021-02-01 17:54:07 +0000

    Updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+// import React from 'react';
 import './App.css';
+
+function Helloworld (props) {
+  return (
+    <div id="hello"> {props.myText}</div>
+  )
+}
+
+
+
 
 function App() {
   return (
     <div>
-      <h1> Hello World</h1>
+      <h1> Hi, this is my first component: <Helloworld myText ="Hello my World"/></h1>
+      <h2><Helloworld myText ="Hi everybody"/></h2>
+      <h3><Helloworld myText="Howdy boy"/></h3>
     </div>
   );
 }
