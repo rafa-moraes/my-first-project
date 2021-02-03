e6e6c39caab08e1cf660c243aa3cf28f95b9cc3c
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2021-02-03 21:20:52 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-// import React from 'react';
+import React from 'react';
 import './App.css';
 
-function Helloworld (props) {
-  return (
-    <div id="hello"> {props.myText}</div>
-  )
-}
+// function Helloworld (props) {
+//   return (
+//     <div id="hello"> 
+//     {props.myText}
+//      </div>
+//   )
+// }
 
+class Helloworld extends React.Component{
+  state = {
+    show: true
+  }
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+  render () {
+    if(this.state.show){
+      return (
+        <div id="hello"> 
+        {this.props.myText}
+        <button onClick={this.toggleShow}> Toggle show</button>
+         </div>
+      )
+    }else {
+      return <h2>No components available <button onClick={this.toggleShow}> Toggle show</button></h2>
+      
+    }
+
+  }
+}
 
 function App() {
   return (
