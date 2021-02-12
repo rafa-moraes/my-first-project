d8ca404af53b79682e87ee1f43c20525902d6b6f
Rafael Moraes Barros <rafael_m_barros@hotmail.com>
2021-02-12 21:05:21 +0000

    update

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -5,26 +5,40 @@ export default class TaskForm extends React.Component {
     
     state = {
         title: "",
-        Description: ""
+        description: ""
     }
 
 
     submit = (e) => {
+        console.log(this.state)
         e.preventDefault()
 
     }
     changes = (e) => {
-    console.log(e.target.value)
+     this.setState({
+         [e.target.name]: e.target.value
+     })
     }
     
     render () {
 
         return (
         <form onSubmit = {this.submit}>
-            <input type="text" placeholder="White a text" onChange ={this.changes} value ={}/>
+            <input 
+            type="text" 
+            placeholder="White a text" 
+            onChange ={this.changes} 
+            name="title" 
+            value ={this.state.title}
+            />
             <br/>
             <br/>
-            <textarea placeholder="Write a description" onChange ={this.changes} value ={}/>
+            <textarea 
+            placeholder="Write a description" 
+            onChange ={this.changes} 
+            name="description" 
+            value ={this.state.description}
+            />
             <button type="submit">Send</button>            
 
 
